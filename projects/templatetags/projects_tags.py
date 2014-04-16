a1eaf7bcd40b96d4074202174af3dab5a0d026e1
Kevin McDermott <kevin@bigkevmcd.com>
2014-04-16 09:58:19 +0100

    Change the project_tags parameter to use build_key which makes it clearer.

--- projects/templatetags/projects_tags.py
+++ projects/templatetags/projects_tags.py
@@ -8,12 +8,12 @@ register = Library()
 
 
 @register.simple_tag()
-def build_url(build_id):
+def build_url(build_key):
     """
     Fetches the ProjectBuild for a given build_id, if any.
     """
     try:
-        build = ProjectBuild.objects.get(build_key=build_id)
+        build = ProjectBuild.objects.get(build_key=build_key)
         return reverse(
             "project_projectbuild_detail",
             kwargs={"project_pk": build.project.pk, "build_pk": build.pk})
