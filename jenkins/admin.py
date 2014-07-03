9effd4fffcd4bd09ff9a24f0ac97f3508b12cab1
David Murphy <dave@schwuk.com>
2014-07-03 14:13:27 +0100

    Added 'Push selected jobs to Jenkins' action to admin for jobs.

--- jenkins/admin.py
+++ jenkins/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 from jenkins.models import JenkinsServer, Job, Build, Artifact, JobType
+from jenkins.tasks import push_job_to_jenkins
 
 
 class BuildAdmin(admin.ModelAdmin):
@@ -10,8 +11,19 @@ class BuildAdmin(admin.ModelAdmin):
     search_fields = ("build_id", "job")
 
 
+class JobAdmin(admin.ModelAdmin):
+    actions = ['push_jobs_to_jenkins']
+
+    def push_jobs_to_jenkins(self, request, queryset):
+        for job in queryset:
+            push_job_to_jenkins.delay(job.pk)
+        self.message_user(request, "Jobs were pushed to Jenkins.")
+
+    push_jobs_to_jenkins.short_description = "Push selected jobs to Jenkins"
+
+
 admin.site.register(JenkinsServer)
-admin.site.register(Job)
+admin.site.register(Job, JobAdmin)
 admin.site.register(JobType)
 admin.site.register(Build, BuildAdmin)
 admin.site.register(Artifact)
